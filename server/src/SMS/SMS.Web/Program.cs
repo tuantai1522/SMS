e244ca7d0cdbd48cd7e71fa0d658e24d21601848
tuantai0708 <tai.bang157@gmail.com>
2025-10-24 18:41:07 +0700

    feat(DI): To add service in Program.cs

--- server/src/SMS/SMS.Web/Program.cs
+++ server/src/SMS/SMS.Web/Program.cs
@@ -1,25 +1,37 @@
+using System.Reflection;
 using SMS.Infrastructure;
 using SMS.UseCases;
 using SMS.Web;
+using SMS.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGenWithAuth();
 
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration)
     .AddWeb(builder.Configuration);
 
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
 var app = builder.Build();
+
+app.MapEndpoints();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwaggerWithUi();
 }
+
+// Use global custom exception handler
+app.UseExceptionHandler();
+
+app.UseCors();            
+app.UseAuthentication();  
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
