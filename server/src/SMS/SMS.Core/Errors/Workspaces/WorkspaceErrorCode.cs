d11f53628f1f1a3165b8298dbcf2c605cd744720
tuantai0708 <tai.bang157@gmail.com>
2025-11-15 17:14:38 +0700

    fix(validation): To add validation when get workspace by Id

--- server/src/SMS/SMS.Core/Errors/Workspaces/WorkspaceErrorCode.cs
+++ server/src/SMS/SMS.Core/Errors/Workspaces/WorkspaceErrorCode.cs
@@ -4,5 +4,6 @@ public static class WorkspaceErrorCode
 {
     public const int NameEmpty = -9000;
     public const int CanNotFindWorkspace = -9001;
+    public const int WorkspaceIdEmpty = -9002;
     
 }
---
+++ server/src/SMS/SMS.UseCases/Features/Workspaces/GetWorkspaceById/GetWorkspaceByIdQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using SMS.Core.Errors.Workspaces;
+
+namespace SMS.UseCases.Features.Workspaces.GetWorkspaceById;
+
+internal sealed class GetWorkspaceByIdQueryValidator : AbstractValidator<GetWorkspaceByIdQuery>
+{
+    public GetWorkspaceByIdQueryValidator()
+    {
+        RuleFor(c => c.Id)
+            .NotEmpty()
+            .WithErrorCode(WorkspaceErrorCode.WorkspaceIdEmpty.ToString())
+            .WithMessage("Workspace Id can not be empty.");
+    }
+}
