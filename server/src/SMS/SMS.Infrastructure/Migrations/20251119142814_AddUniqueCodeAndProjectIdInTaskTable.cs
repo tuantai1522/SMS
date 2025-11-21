1d1b093951329cb936b4c8db5bd6af23952fe58b
tuantai0708 <tai.bang157@gmail.com>
2025-11-21 19:46:33 +0700

    feat(domain): To add unique violation constraints of task code in Project

---
+++ server/src/SMS/SMS.Infrastructure/Migrations/20251119142814_AddUniqueCodeAndProjectIdInTaskTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueCodeAndProjectIdInTaskTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_tasks_code_project_id",
+                schema: "sms",
+                table: "tasks",
+                columns: new[] { "code", "project_id" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_tasks_code_project_id",
+                schema: "sms",
+                table: "tasks");
+        }
+    }
+}
