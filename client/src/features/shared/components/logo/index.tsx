2294d7e1d408110ab6862877b30b9011e9110914
tuantai0708 <tai.bang157@gmail.com>
2025-12-03 20:07:07 +0700

    feat(component): To add shared components

---
+++ client/src/features/shared/components/logo/index.tsx
@@ -0,0 +1,16 @@
+import { AudioWaveform, Link } from "lucide-react";
+
+const Logo = (props: { url?: string }) => {
+  const { url = "/" } = props;
+  return (
+    <div className="flex items-center justify-center sm:justify-start">
+      <Link to={url}>
+        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <AudioWaveform className="size-4" />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
